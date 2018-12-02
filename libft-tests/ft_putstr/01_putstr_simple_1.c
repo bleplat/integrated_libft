79aab7818b16b0f7a274431ed57fd0b64590c8e9
Your Name <bleplat@student.42.fr>
2018-12-02 23:17:05 +0100

    fixed tests

--- libft-tests/ft_putstr/01_putstr_simple_1.c
+++ libft-tests/ft_putstr/01_putstr_simple_1.c
@@ -6,7 +6,7 @@
 /*   By: fbabin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 23:46:06 by fbabin            #+#    #+#             */
-/*   Updated: 2018/12/02 21:07:36 by fbabin           ###   ########.fr       */
+/*   Updated: 2018/12/02 23:06:05 by bleplat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int		test_putstr_simple_1(void)
 	int		p_fd[2];
 	char	buff1[100];
 
+	bzero(buff1, 100);
 	stdout_fd = ut_get_stdout_fd(p_fd);
 	ft_putstr("hello");
 	ut_get_stdout_buffer(100, buff1, stdout_fd, p_fd);
